8af0bcb2268900ca20f88dbd106d5eb58b57d002
Oevo <team@oevo.com>
2018-12-08 17:11:30 +0600

    rs

--- app/src/main/java/com/resatisfy/push/MainActivity.java
+++ app/src/main/java/com/resatisfy/push/MainActivity.java
@@ -3,9 +3,16 @@ package com.resatisfy.push;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
 import com.resatisfy.android_lib.RSPush;
+import com.resatisfy.android_lib.utilities.RSConfig;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,8 +25,12 @@ public class MainActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(getApplicationContext());
         RSPush.takeOff(getApplicationContext());
 
+
+
+//        RSConfig getConfig = RSConfig.defaultConfig(getApplicationContext());
+//
 //        System.out.println("---------------------------------AS");
-//        System.out.println(RSPush.channelId(getApplicationContext()));
+//        System.out.println(getConfig.getAppKey());
 
         //this.deactiveChannel(RSPush.channelId(getApplicationContext()));
 
